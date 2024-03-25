6ed5273ce2730f79ebc6232f86abd217087b0308
Sanghitam101 <164131140+Sanghitam101@users.noreply.github.com>
2024-03-25 17:52:26 +0800

    Create Kp.py

---
+++ Kp.py
@@ -0,0 +1,84 @@
+!#usr/bin/python
+import sys
+import time
+def slowprint (s):
+    for c in s + '\n':
+        sys.stdout.write (c)
+        sys.stdout.flush ()
+        time.sleep (1. /60)
+        
+input ('masukan nama :')        
+input ('masukan umur ;')
+
+
+slowprint ( '''\33[1;35m
+┏━━┓────────┏━━┓┏┓───── 
+┃┏┓┃────────┃┏┓┃┃┃───── 
+┃┗┛┃┏━━┓┏━━┓┃┗┛┃┃┃─┏━━┓ 
+┗━┓┃┃┏┓┃┃┏┓┃┗━┓┃┃┃─┃┃━┫ 
+┏━┛┃┃┗┛┃┃┗┛┃┏━┛┃┃┗┓┃┃━┫ 
+┗━━┛┗━━┛┗━━┛┗━━┛┗━┛┗━━┛ 
+''')
+print ('\33[39m_'*50)
+print ('\n')
+slowprint ('\33[37m[\33[1;34m+\33[37m]\33[;35mFACEBOOK : KAK FARDI')
+slowprint  ('\33[37m[\33[1;34m0\33[37m]\33[;35mWA   : 08521704558X')
+slowprint  ('\33[37m[\33[1;34m✓\33[37m]\33[;35mABIS PAKAI SC INI JNGAN RUSAKIN ANJING ')
+print ('         \33[37m     follow github me ')
+print ('\n')
+
+print ('\33[39m_'*50)
+print ('\n')
+slowprint ('\33[1;30m[\33[31m01\33[1;30m]\33[1;36mID TARGET FECBOK')
+slowprint ('\33[1;30m[\33[31m02\33[1;30m]\33[1;36mCLIKER RANDOM')
+slowprint ('\33[1;30m[\33[31m03\33[1;30m]\33[1;36mGRUP ID CLIKER')
+slowprint ('\33[1;30m[\33[31m04\33[1;30m]\33[1;36mUPGRADE TOLS PACKAGE')
+slowprint ('\33[1;30m[\33[31m00\33[1;30m]\33[1;36m EXIT ')
+print ('\33[39m_'*50)
+print ('\n')
+print ('\33[1;31...........\33[32m●\33[33m●\33[34m●\33[35m●\33[36m●')
+
+input ('\33[1;30m[\33[31m✓\33[1;30m]\33[1;34mMASUKAN PILIHAN \33[39m: ')
+
+print ('\33[1;30mharap jangan mengetik apa pun ')
+print ('\n')
+print ('\33[1;31mproses..\33[32m●\33[33m●\33[34m●\33[35m●\33[36m●')
+import sys
+import time
+def slowprint (s):
+    for c in s + '\n':
+        sys.stdout.write (c)
+        sys.stdout.flush ()
+        time.sleep (1. /60)
+        
+input ('masukan nama :')        
+input ('masukan umur ;')
+
+
+slowprint ( '''\33[1;35m
+┏━━┓────────┏━━┓┏┓───── 
+┃┏┓┃────────┃┏┓┃┃┃───── 
+┃┗┛┃┏━━┓┏━━┓┃┗┛┃┃┃─┏━━┓ 
+┗━┓┃┃┏┓┃┃┏┓┃┗━┓┃┃┃─┃┃━┫ 
+┏━┛┃┃┗┛┃┃┗┛┃┏━┛┃┃┗┓┃┃━┫ 
+┗━━┛┗━━┛┗━━┛┗━━┛┗━┛┗━━┛ 
+''')
+print ('\33[39m_'*50)
+print ('\n')
+slowprint ('\33[37m[\33[1;34m+\33[37m]\33[;35mFACEBOOK : KAK FARDI')
+slowprint  ('\33[37m[\33[1;34m0\33[37m]\33[;35mWA   : 08521704558X')
+slowprint  ('\33[37m[\33[1;34m✓\33[37m]\33[;35mABIS PAKAI SC INI JNGAN RUSAKIN ANJING ')
+print ('         \33[37m     follow github me ')
+print ('\n')
+
+print ('\33[39m_'*50)
+print ('\n')
+slowprint ('\33[1;30m[\33[31m01\33[1;30m]\33[1;36mID TARGET FECBOK')
+slowprint ('\33[1;30m[\33[31m02\33[1;30m]\33[1;36mCLIKER RANDOM')
+slowprint ('\33[1;30m[\33[31m03\33[1;30m]\33[1;36mGRUP ID CLIKER')
+slowprint ('\33[1;30m[\33[31m04\33[1;30m]\33[1;36mUPGRADE TOLS PACKAGE')
+slowprint ('\33[1;30m[\33[31m00\33[1;30m]\33[1;36m EXIT ')
+print ('\33[39m_'*50)
+print ('\n')
+print ('\33[1;31...........\33[32m●\33[33m●\33[34m●\33[35m●\33[36m●')
+
